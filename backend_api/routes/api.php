2c1fc4e0956f1c61053db7a6c4854f921fbca106
daief48 <daief20103048@gmail.com>
2025-12-15 00:12:34 +0600

    create controller and route for get single product

--- backend_api/routes/api.php
+++ backend_api/routes/api.php
@@ -7,4 +7,5 @@ use App\Http\Controllers\Api\ProductController;
 
 Route::prefix('products')->name('products.')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('index');
+        Route::get('{product}', [ProductController::class, 'show'])->name('show');
 });
